636ca5be4b298719fba482c2714495b729ba8c3d
alantrrs <alan.trrs.ngls@gmail.com>
2018-08-09 11:14:52 -0700

    WIP - request file upload urls.

--- tensordash/main.py
+++ tensordash/main.py
@@ -2,42 +2,80 @@
 from warrant import Cognito
 import configparser
 import os
+from gql import Client, gql
+from gql.transport.requests import RequestsHTTPTransport
+import mimetypes
 
 class Tensordash():
     def __init__(self):
-        self.poolId = 'us-east-1_fc6CHd37U'
-        self.clientId = '62hrpekqtvh8v5u3u1bipos574'
+        self.poolId = 'us-east-1_LfXdV62Vn'
+        self.clientId = '6deesjtjflk6aphp4dmt9pl8d9'
+        self.graphqlApiUrl = 'https://jgyool6savhb3lyvsxcmdivcw4.appsync-api.us-east-1.amazonaws.com/graphql'
         # Load user config
         self.config = configparser.ConfigParser()
         self.configpath = '.tensordash.cfg'
         if os.path.exists(self.configpath):
             self.config.read(self.configpath)
+            if 'AUTH' in self.config.sections():
+                self._refresh_auth()
 
     def push(self, project, filepaths, user=None, password=None):
-        self._authenticate(user, password)
-        # TODO: Get signed URLs for all files
-        print('PUSH:', project, filepaths)
-        # TODO: Upload files from directory to S3 directly
+        if user is not None:
+            self._authenticate(user, password)
+        # TODO: Create new experiment
+        # Get signed URLs for all files
+        assets = map(lambda asset: {
+            'localUri': asset,
+            'mimeType': mimetypes.guess_type(asset)
+        }, filepaths)
+        query = gql('''
+            query requestAssets($assetsInput: AssetsInput!) {
+                requestAssetsUpload(input: $assetsInput)
+            }
+        ''')
+        params = {'assetsInput': {'assets': assets}}
+        res = self.client.execute(query, variable_values=params)
+        print(res)
+        # TODO: Upload files to S3 directly
         # TODO: Update DB with outputs
-        print("pushing..")
+
+    def _refresh_auth(self):
+        # TODO: Check if the credentials are set
+        auth = self.config['AUTH']
+        u = Cognito(self.poolId, self.clientId, id_token=auth['id_token'],
+                    access_token=auth['access_token'], refresh_token=auth['refresh_token'])
+        u.check_token()
+        # save config in memory
+        self.config['AUTH'] = {'id_token': u.id_token,
+                               'access_token': u.access_token,
+                               'refresh_token': u.refresh_token}
+        # Reauth GraphQL client
+        self._update_graphql_client(u.access_token)
 
     def _authenticate(self, user, password):
         try:
             u = Cognito(self.poolId, self.clientId, username=user)
             u.authenticate(password=password)
+            # save config in memory
+            self.config['AUTH'] = {'id_token': u.id_token,
+                               'access_token': u.access_token,
+                               'refresh_token': u.refresh_token}
+            # Setup GraphQL client
+            self._update_graphql_client(u.access_token)
         except:
             print('Not authorized. Incorrect username or password.')
-            return (None, None, None)
-        return (u.id_token, u.access_token, u.refresh_token)
+            exit(1)
+
+    def _update_graphql_client(self, access_token):
+        self.client = Client(transport=RequestsHTTPTransport(
+            url=self.graphqlApiUrl,
+            headers={'Authorization': access_token},
+            use_json=True
+        ))
 
     def login(self, user, password):
-        id_token, access_token, refresh_token = self._authenticate(user, password)
-        if id_token is None:
-            exit(1)
-        # save the config
-        self.config['AUTH'] = {'id_token': id_token,
-                               'access_token': access_token,
-                               'refresh_token': refresh_token}
+        self._authenticate(user, password)
+        # write auth to file
         self.config.write(open(self.configpath, 'w'))
         print('You\'re logged in. Credentials stored.')
 
