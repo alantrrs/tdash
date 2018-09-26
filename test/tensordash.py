9eca48e83a6563c1ccce913d83cfd05b40a58b67
alantrrs <alan.trrs.ngls@gmail.com>
2018-09-25 17:09:22 -0700

    Add tests

---
+++ test/tensordash.py
@@ -0,0 +1,22 @@
+
+import unittest
+from tensordash.main import Tensordash
+import os
+
+class TensordashTestCase(unittest.TestCase):
+    def setUp(self):
+        self.tensordash = Tensordash()
+# Test config
+class ConfigurationTestCase(TensordashTestCase):
+    def runTest(self):
+        self.tensordash.reconfigure()
+
+# Test login
+class LoginTestCase(TensordashTestCase):
+    def runTest(self):
+        self.tensordash.login('test', os.getenv('TEST_PASSWORD'))
+
+# Test push
+# Test logout
+if __name__ == '__main__':
+    unittest.main()
